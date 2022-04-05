b0faacb50d13a54e4276ca0537bfc8801f8065a7
Ítalo Fernandes <italo.avelino@outlook.com.br>
2022-04-05 00:42:58 -0300

    Adicionando funcionalidade que interpreta requisições urlencoded e json

--- config/customExpress.js
+++ config/customExpress.js
@@ -3,6 +3,9 @@ const consign = require("consign");
 
 module.exports = () => {
     const app = express();
+
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
     
     consign()
         .include("controllers")
--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const customExpress = require("./config/customExpress");
 const app = customExpress();
 
 app.listen(8081, () => {
-    console.log("Servidor de rodando na porta: http://localhost/8081");
+    console.log("Servidor de rodando na porta: http://localhost:8081");
     console.log("Para derrubar o servidor: CTRL + C no console");
 });
