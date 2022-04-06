8ef2326aab75ea8996601b1b0f7821d1acf788ad
Ítalo Fernandes <italo.avelino@outlook.com.br>
2022-04-05 23:00:29 -0300

    Adicionando conexão com o banco de dados MySQL

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const customExpress = require("./config/customExpress");
+const conexao = require("./infraestrutura/conexao");
 
-const app = customExpress();
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro);
+    }else {
+        console.log("Conectado com sucesso");
 
-app.listen(8081, () => {
-    console.log("Servidor de rodando na porta: http://localhost:8081");
-    console.log("Para derrubar o servidor: CTRL + C no console");
+        const app = customExpress();
+
+        app.listen(8081, () => {
+            console.log("Servidor de rodando na porta: http://localhost:8081");
+            console.log("Para derrubar o servidor: CTRL + C no console");
+        }); 
+    }
 });
+
