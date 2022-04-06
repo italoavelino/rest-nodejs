df3395ccfd9b0ac12e0017ab167efd7376191ae5
Ítalo Fernandes <italo.avelino@outlook.com.br>
2022-04-06 20:58:01 -0300

    Criando classe que insere dados na tabela

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,13 @@
+const Atendimento = require("../models/atendimentos");
+
 module.exports = app => {
     app.get("/atendimentos", (req, res) => {
         res.send("<h1>Você está na rota de atendimentos e está realizando um GET</h1>");
     });
 
     app.post("/atendimentos", (req, res) => {
-        console.log(req.body);
-        res.send("<h1>Você está na rota de atendimentos e está realizando um POST</h1>");
+        console.log("Atendimento enviado");
+        Atendimento.adiciona(req.body);
+        res.send("Post atendimento");
     });
 };
