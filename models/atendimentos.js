c55f9ba6095e63d26f721ddab4bc4ea142a98ea8
Ítalo Fernandes <italo.avelino@outlook.com.br>
2022-04-07 23:09:00 -0300

    Criando validação de nome e data

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -6,17 +6,41 @@ class Atendimento {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format("YYYY-MM-DD");
         const data =  moment(atendimento.data, "DD/MM/YYYY").format("YYYY-MM-DD HH:MM:SS");
-        const atendimentoDatado = {...atendimento, dataCriacao, data};
 
-        const sql = "INSERT INTO Atendimentos SET ?";
+        const dataValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteValido = atendimento.cliente.length >= 5;
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro);
-            } else {
-                res.status(201).json(resultados);
-            };
-        });
+        const validacoes = [
+            {
+                nome: "data",
+                valido: dataValida,
+                mensagem: "Data deve ser maior ou igual a data atual"
+            },
+            {
+                nome: "cliente",
+                valido: clienteValido,
+                mensagem: "Cliente deve ter pelo menos cinco caracteres"
+            }
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if(existemErros) {
+            res.status(400).json(erros);
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data};
+    
+            const sql = "INSERT INTO Atendimentos SET ?";
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro);
+                } else {
+                    res.status(201).json(resultados);
+                };
+            });
+        };
     };
 };
 
